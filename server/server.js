1f766f654a880f6043d06dec150751ba4707dd59
Tokhalyan <tokhalyan@gmail.com>
2022-06-04 13:52:36 -0700

    bug fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./server/config/connection');
+const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
